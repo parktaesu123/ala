9bdba7653959c78b5c0a900b53cceefcafd82983
troy96475 <taesupark0504@gmail.com>
2025-04-16 08:12:29 +0900

    feat :: controller

--- src/main/java/com/example/ala/controller/TrafficController.java
+++ src/main/java/com/example/ala/controller/TrafficController.java
@@ -1,2 +1,22 @@
-package com.example.ala.controller;public class TrafficController {
+package com.example.ala.controller;
+
+import com.example.ala.service.TrafficService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TrafficController {
+    private final TrafficService trafficService;
+
+    @PostMapping("/visit")
+    public void countVisit() {
+        trafficService.visit();
+    }
+
+    @PostMapping("/download")
+    public void countDownload() {
+        trafficService.download();
+    }
 }
