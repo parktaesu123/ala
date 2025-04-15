79a88426ab8d0efaff3f8242b4072b729eebc6f7
troy96475 <taesupark0504@gmail.com>
2025-04-16 08:12:22 +0900

    feat :: service

--- src/main/java/com/example/ala/service/TrafficService.java
+++ src/main/java/com/example/ala/service/TrafficService.java
@@ -1,2 +1,25 @@
-package com.example.ala.service;public class TrafficService {
+package com.example.ala.service;
+
+import com.example.ala.domain.Traffic;
+import com.example.ala.domain.repository.TrafficRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrafficService {
+    private final TrafficRepository trafficRepository;
+
+    @Transactional
+    public void visit() {
+        Traffic traffic = trafficRepository.findById(1L).orElseThrow(); // 고정 ID
+        traffic.visit(); // 증가
+    }
+
+    @Transactional
+    public void download() {
+        Traffic traffic = trafficRepository.findById(1L).orElseThrow(); // 고정 ID
+        traffic.download();
+    }
 }
