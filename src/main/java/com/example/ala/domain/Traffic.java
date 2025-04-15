5ff550496d598b994d65656efdd97ec8c06687b1
troy96475 <taesupark0504@gmail.com>
2025-04-16 08:12:07 +0900

    feat :: traffic domain

--- src/main/java/com/example/ala/domain/Traffic.java
+++ src/main/java/com/example/ala/domain/Traffic.java
@@ -1,2 +1,29 @@
-package com.example.ala.domain;public class Traffic {
+package com.example.ala.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Traffic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    private int visitCount = 0;
+
+    private int downloadCount = 0;
+
+    public void visit() {
+        visitCount++;
+    }
+
+    public void download() {
+        downloadCount++;
+    }
 }
