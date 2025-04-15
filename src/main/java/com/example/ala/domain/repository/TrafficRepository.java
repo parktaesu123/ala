019944594e314ea74a93f15e902a9bf656b736cc
troy96475 <taesupark0504@gmail.com>
2025-04-16 08:12:16 +0900

    feat :: repository

--- src/main/java/com/example/ala/domain/repository/TrafficRepository.java
+++ src/main/java/com/example/ala/domain/repository/TrafficRepository.java
@@ -1,2 +1,7 @@
-package com.example.ala.domain.repository;public interface TrafficRepository {
+package com.example.ala.domain.repository;
+
+import com.example.ala.domain.Traffic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrafficRepository extends JpaRepository<Traffic, Long> {
 }
