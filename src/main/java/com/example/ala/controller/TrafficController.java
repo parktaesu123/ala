5b900db73df0fc745db32830e98a3a1985d1adbd
troy96475 <taesupark0504@gmail.com>
2025-04-16 14:51:01 +0900

    feat :: 전체조회 api

--- src/main/java/com/example/ala/controller/TrafficController.java
+++ src/main/java/com/example/ala/controller/TrafficController.java
@@ -1,17 +1,21 @@
 package com.example.ala.controller;
 
+import com.example.ala.dto.response.TrafficResponse;
+import com.example.ala.service.QueryTrafficService;
 import com.example.ala.service.TrafficService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TrafficController {
     private final TrafficService trafficService;
+    private final QueryTrafficService queryTrafficService;
 
-    @GetMapping("/visit")
+    @GetMapping("")
     public void countVisit() {
         trafficService.visit();
     }
@@ -19,5 +23,10 @@ public class TrafficController {
     @GetMapping("/download")
     public void countDownload() {
         trafficService.download();
+    }
+
+    @GetMapping("/check")
+    public List<TrafficResponse> checkTraffic() {
+        return queryTrafficService.query();
     }
 }
