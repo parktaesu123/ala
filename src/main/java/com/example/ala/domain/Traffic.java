81a092506dd8e8fab0d19454d2fb6064a4d7252e
troy96475 <taesupark0504@gmail.com>
2025-04-16 14:50:32 +0900

    refactor :: getter 추가

--- src/main/java/com/example/ala/domain/Traffic.java
+++ src/main/java/com/example/ala/domain/Traffic.java
@@ -2,9 +2,11 @@ package com.example.ala.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Traffic {
